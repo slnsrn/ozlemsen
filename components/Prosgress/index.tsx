1e5451d5ab8f702108669d22ec79274551046b46
slnsrn <slnsrn@gmail.com>
2022-08-22 13:38:47 +0200

    remove unused import

--- components/Prosgress/index.tsx
+++ components/Prosgress/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useGameContext } from "../../context/GameContext";
 import { GameState } from "../../types";
 
